6501e93cd28ab15f87ee09e533232cbeaafd31e6
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-01-17 12:32:18 -0300

    Awnser control

--- pick-a-color/src/App.tsx
+++ pick-a-color/src/App.tsx
@@ -6,15 +6,26 @@ function App() {
   const 
     [color, setColor] = useState<string>("blue"),
     [awnser, setAwnser] = useState<string[]>([]),
-    defaultColors = ["Black","Blue","Yellow"];
+    [correctAwnser, setCorrectAwnser] = useState<boolean>(false),
+    defaultColors = ["Black","Blue","Yellow"],
+    getRandomColor = () => {
+      return colors[Math.floor(Math.random()* colors.length)];
+    }
  
   useEffect(() => {
-    let actualColor = colors[Math.floor(Math.random()* colors.length)];
+    let actualColor = getRandomColor()
     setColor(actualColor.color);
 
   },[])
 
-  const checkAnwser = () => {
+  const checkAnwser = (colorValue: string) => {
+    console.log("checkAnwser:",color);
+    if(colorValue === color){
+      console.log("Correta!")
+      console.log("gerar nova cor!");
+      let newColor = getRandomColor();
+      setColor(newColor.color);
+    }
 
   }
 
@@ -24,9 +35,12 @@ function App() {
 
       </div>
       <div className='colorOptions'>
-        {defaultColors.map((color) => {
-          return <button id={color}>{color}</button>
+        {defaultColors.map((colorValue) => {
+          return <button id={colorValue} onClick={() => checkAnwser(colorValue)}>{colorValue}</button>
         })}
+      </div>
+      <div className='Awnser'>
+        {!correctAwnser ? <h3>Wrong Awnser!</h3> :<h3> Correct Awnser!</h3>}
       </div>
     </div>
   )
