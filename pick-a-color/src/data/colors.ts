24ae26842580ba4dbb9f2f032d96a8cf4c22090a
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-01-17 13:32:42 -0300

    Strict mode out and added colors

--- pick-a-color/src/data/colors.ts
+++ pick-a-color/src/data/colors.ts
@@ -1,16 +1,9 @@
-const colors = [
-    {
-        color : "#FFFFFF",
-    },
-    {
-        color : "#C0C0C0",
-    },
-    {
-        color : "#808080"
-    }
-]
+// const colors = [
+//     "#FFFFFF",
+//     "#C0C0C0",
+//     "#808080"
+// ]
+
+const colors = ["Black","Blue","Yellow","Red","Maroon","Olive","Lime","Green","Aqua","Teal","Navy","Fuchsia","Purple"];
    
-
-
-
-export default colors
+export default colors;
